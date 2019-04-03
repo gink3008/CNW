c2b8fcb3c8b28f4e1137d96a76e3c3fd07d8f545
hiep le hoang <authou@outlook.com>
2019-04-03 08:03:19 +0700

    commit model + view

--- CNW/Models/ProductModel.cs
+++ CNW/Models/ProductModel.cs
@@ -12,11 +12,47 @@ namespace CNW.Models
         {
             context = new Model1();
         }
-        public List<SanPham> accessDatabase()
+        public IQueryable<SanPham> accessDatabase
         {
-            List<SanPham> exam = new List<SanPham>();
-            return exam;
+           get { return context.SanPhams;}
         }
+        public SanPham FindSanPham(int MaSP)
+        {
+            return context.SanPhams.Find(MaSP);
+        }
+        public bool Insert(SanPham model)
+        {
+            SanPham dbSanPham = context.SanPhams.Find(model.id);
+            if (dbSanPham.id != null)
+            {
+                return false;
+            }
+            else
+            {
+                context.SanPhams.Add(model);
+                return true;
+            }
+            return false;
+        }
+        public bool Update(SanPham model)
+        {
+            SanPham dbSanPham = context.SanPhams.Find(model.id);
+            if (dbSanPham != null)
+            {
+                dbSanPham.Name = model.Name;
 
+            }
+         
+            return false;
+        }
+        public bool Delete(SanPham model)
+        {
+            SanPham dbSanPham = context.SanPhams.Find(model.id);
+            if (dbSanPham != null)
+            {
+                context.SanPhams.Remove(dbSanPham);
+            }
+            return false;
+        }
     }
 }
