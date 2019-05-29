f1443cbe730d7d762b7ee2e6685bec01f0e2326a
hiep le hoang <authou@outlook.com>
2019-05-30 02:29:32 +0700

    commit slibar

---
+++ CNW/Areas/AdminArea/Controllers/SizeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNW.Areas.AdminArea.Controllers
+{
+    public class SizeController : Controller
+    {
+        // GET: AdminArea/Size
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
